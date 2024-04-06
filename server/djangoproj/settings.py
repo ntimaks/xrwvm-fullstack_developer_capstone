559a27241288d927d58f938f3446f2b61e634614
ntimaks <ntimaks27@gmail.com>
2024-04-06 08:21:11 -0400

    A descriptive message about what you've changed

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS=['localhost','https://ntimaks27-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://ntimaks27-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = [
+    'https://ntimaks27-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://ntimaks27-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
